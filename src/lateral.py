efd85b6079b69705aaf3ec09d9e23eb08548aea8
Francesco Turci <turci@me.com>
2022-08-09 10:45:05 +0100

    Updated lateral.py.

---
+++ src/lateral.py
@@ -0,0 +1,7 @@
+import ovito
+import numpy as np
+import argparse
+from analyser import Quadrant
+
+Q = Quadrant("Analyse the density difference in the four quadrants of the plane orthogonal to z")
+Q.compute()
