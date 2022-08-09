79032c8629f7bfe9f62a58984fa380a0bc1866c6
Francesco Turci <turci@me.com>
2022-08-09 10:45:59 +0100

    Updated lateral.py.

--- src/lateral.py
+++ src/lateral.py
@@ -3,5 +3,5 @@ import numpy as np
 import argparse
 from analyser import Quadrant
 
-Q = Lateral("Analyse the density difference in the four quadrants of the plane orthogonal to z")
+Q = Lateral()
 Q.compute()
