134538ca6a3f5a906281eceb01a52bad3b068155
Francesco Turci <turci@me.com>
2022-08-09 10:48:22 +0100

    Updated lateral.py.

--- src/lateral.py
+++ src/lateral.py
@@ -1,9 +1,9 @@
 import ovito
 import numpy as np
 import argparse
-from analyser import Quadrant
+import analyser
 
-L = Lateral()
+L = analyser.Lateral()
 L.compute()
 L.stats()
 print(L.avg_profile)
